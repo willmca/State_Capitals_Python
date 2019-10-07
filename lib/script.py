d3bf233204970a3eb37af72b8f884c6f477d9841
Will McAllister <willmca32@gmail.com>
2019-10-07 14:21:59 -0400

    MVP

--- lib/script.py
+++ lib/script.py
@@ -3,18 +3,29 @@ import random
 correct = 0
 incorrect = 0
 
-print("Hello! Would you like to play a game? Type yes to begin playing guess that state capital")
-answer = str(input())
-if answer == "yes":
+def game_play():
+    correct = 0
+    incorrect = 0
     random.shuffle(states)
     for x in states:
         print(f"What is the capital of {x['name']}?")
         answer2 = str(input())
         if answer2 == x['capital']:
-                correct = correct +1
-                print(f"Correct! Your current score is {correct} correct answers and {incorrect} incorrect answers")
+            correct = correct + 1
+            incorrect = incorrect
+            print(
+                f"Correct! Your current score is {correct} correct answers and {incorrect} incorrect answers")
         else:
-            incorrect = incorrect +1
-            print(f"Sorry, that wasn't right. The answer was {x['capital']}. Your current score is {correct} correct answers and {incorrect} incorrect answers")
-else: 
-    print("Maybe next time")
+            correct = correct
+            incorrect = incorrect + 1
+            print(
+                f"Sorry, that wasn't right. The answer was {x['capital']}. Your current score is {correct} correct answers and {incorrect} incorrect answers")
+        if incorrect + correct == 50:
+            print("Game over, try again?")
+            answer3 = str(input())
+            if answer3 == "yes":
+                game_play()
+                print("Let's do it!")
+            else:
+                print("Thanks for playing!")
+game_play()
