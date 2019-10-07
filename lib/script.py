1768ad80ffa7d14d7ab0425d3e3767fcd7801bcc
Will McAllister <willmca32@gmail.com>
2019-10-07 13:53:13 -0400

    Everything works outside of repeating game at end

--- lib/script.py
+++ lib/script.py
@@ -1,3 +1,20 @@
 from capitals import states
+import random
+correct = 0
+incorrect = 0
 
-print(states)
+print("Hello! Would you like to play a game? Type yes to begin playing guess that state capital")
+answer = str(input())
+if answer == "yes":
+    random.shuffle(states)
+    for x in states:
+        print(f"What is the capital of {x['name']}?")
+        answer2 = str(input())
+        if answer2 == x['capital']:
+                correct = correct +1
+                print(f"Correct! Your current score is {correct} correct answers and {incorrect} incorrect answers")
+        else:
+            incorrect = incorrect +1
+            print(f"Sorry, that wasn't right. The answer was {x['capital']}. Your current score is {correct} correct answers and {incorrect} incorrect answers")
+else: 
+    print("Maybe next time")
